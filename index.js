e5903fcc317a2500bb68a45f3e04f9c5e10fc209
Fiorella <fiorella2994s@gmail.com>
2021-04-19 21:40:14 -0300

    added library and server

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-const http = require('http')
-const { request } = require('node:http')
+//*create server*//
+const express = require('express')
+const app = express()
+//*libary*//
+const bodyParser = require("body-parser");
+const cors = require('cors');
+//* server *//
+const port = 4000
 
-const server = http.createServer((request, response) =>{
-    console.log(request.url)
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-    response
+const user = {
+  email: '',
+  name: '',
+  password: ''
+}
+
+app.post('/register', (req, res) => {
+  user.email = req.body.email
+  user.name = req.body.name
+  user.password = req.body.password
+  res.send({ registered: user });
+})
+
+app.put('/login', (req, res) => {
+  const verifyUser = {
+    email: '',
+    password: ''
+  }
+  verifyUser.email = req.body.email
+  verifyUser.password = req.body.password
+
+  if (verifyUser.email === user.email && verifyUser.password == user.password) {
+    res.send({ logged: verifyUser })
+  }
+  else {
+    res.status(404).send({ error: 'Invalid credentials' })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
